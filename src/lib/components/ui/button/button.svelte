d846571acf90808d6ef70ac9bcb62f1157e773c5
Saix <bark@bigstinky.dog>
2024-10-26 20:18:11 -0400

    migrate to svelte 5

--- src/lib/components/ui/button/button.svelte
+++ src/lib/components/ui/button/button.svelte
@@ -1,25 +1,35 @@
 <script lang="ts">
 import { cn } from '$lib/utils.js';
 import { Button as ButtonPrimitive } from 'bits-ui';
-import { type Events, type Props, buttonVariants } from './index.js';
+import type { Snippet } from 'svelte';
+import { type Props as ButtonProps, type Events, buttonVariants } from './index.js';
 
-type $$Props = Props;
-type $$Events = Events;
+interface Props {
+  class?: ButtonProps['class'];
+  variant?: ButtonProps['variant'];
+  size?: ButtonProps['size'];
+  builders?: ButtonProps['builders'];
+  children?: Snippet;
+  // [key: string]: any
+}
 
-let className: $$Props['class'] = undefined;
-export let variant: $$Props['variant'] = 'default';
-export let size: $$Props['size'] = 'default';
-export let builders: $$Props['builders'] = [];
-export { className as class };
+let {
+  class: className = undefined,
+  variant = 'default',
+  size = 'default',
+  builders = [],
+  children,
+  ...rest
+}: Props = $props();
 </script>
 
 <ButtonPrimitive.Root
   {builders}
   class={cn(buttonVariants({ variant, size, className }))}
   type="button"
-  {...$$restProps}
+  {...rest}
   on:click
   on:keydown
 >
-  <slot />
+  {@render children?.()}
 </ButtonPrimitive.Root>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
-<script>
+<script lang="ts">
+import type { Snippet } from 'svelte';
 import '../app.css';
+
+/** @type {Props} */
+interface Props {
+  children?: Snippet;
+}
+let { children } = $props();
 </script>
 
-<slot></slot>
+{@render children?.()}
